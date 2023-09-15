c6f0254994266b3a76f4bee469c4edef2d81567b
prandeveloper <pranaydevelopersveltosest@gmail.com>
2023-09-15 10:45:35 +0530

    update

--- src/components/pages/apply/plans/BmiPlans.js
+++ src/components/pages/apply/plans/BmiPlans.js
@@ -106,7 +106,7 @@ export default function ApplyList() {
                                     className="d-flex selectplan"
                                   >
                                     <span style={{ padding: "2.5rem" }}>
-                                      VISITOR INFORMATION
+                                      VISITOR
                                     </span>
                                     <img src={step} alt="image" />
                                   </div>
@@ -200,16 +200,6 @@ export default function ApplyList() {
                                           <option value="Pre-ex Coverage Not Included">
                                             Pre-ex Coverage Not Included
                                           </option>
-                                          {/* {this.state.scriptN?.map(
-                                            allScript => (
-                                              <option
-                                                value={allScript?._id}
-                                                key={allScript?._id}
-                                              >
-                                                {allScript?.scriptName}
-                                              </option>
-                                            )
-                                          )} */}
                                         </CustomInput>
                                       </div>
                                       <div>
@@ -223,7 +213,6 @@ export default function ApplyList() {
                                           className="m-2"
                                         />
                                         <label for="Yes">Yes</label>
-                                        {/* <Label>Yes</Label> */}
                                         <Input
                                           type="radio"
                                           id="html"
@@ -246,74 +235,6 @@ export default function ApplyList() {
                                           onChange={handleChange}
                                         />
                                       </div>
-                                      {/* <div className="my-2 mb-4">
-                                        <label>Select Plan</label>
-
-                                        <select
-                                          name="Select Plan"
-                                          onChange={handleChange}
-                                          aria-label="Default select example"
-                                        >
-                                          <option selected>Select Plan</option>
-                                          <option value="1">
-                                            INF Standard
-                                          </option>
-                                          <option value="2">
-                                            INF SAFE TRAVELLER USA 90
-                                          </option>
-                                          <option value="3">
-                                            INF SAFE TRAVELLER USA
-                                          </option>
-                                          <option value="4">
-                                            INF PREMIER PLUS
-                                          </option>
-                                          <option value="5">INF PREMIER</option>
-                                          <option value="6">
-                                            INF ELITE PLUS
-                                          </option>
-                                          <option value="7">
-                                            INF ELITE 90
-                                          </option>
-                                          <option value="8">INF ELITE </option>
-                                        </select>
-                                      </div> */}
-                                      {/* <div>
-                                        <div className="my-2 mb-4">
-                                          <h5 className="py">
-                                            POLICY MAXIMUM:
-                                          </h5>
-                                          <select
-                                            name="POLICY MAXIMUM"
-                                            onChange={handleChange}
-                                          >
-                                            <option selected>
-                                              Select Plan
-                                            </option>
-                                            <option value="1">
-                                              INF Standard
-                                            </option>
-                                            <option value="2">
-                                              INF SAFE TRAVELLER USA 90
-                                            </option>
-                                          </select>
-                                        </div>
-                                      </div> */}
-                                      {/* <div>
-                                        <div className="my-2 mb-4">
-                                          <h5 className="py">
-                                            PREEX DEDUCTIBLE/ MAX:
-                                          </h5>
-                                          <select
-                                            name="PREEX DEDUCTIBLE/MAX"
-                                            onChange={handleChange}
-                                            aria-label="Default select example"
-                                          >
-                                            <option selected>
-                                              Select Plan
-                                            </option>
-                                          </select>
-                                        </div>
-                                      </div> */}
 
                                       <div className="mt-2">
                                         <CustomInput
@@ -335,16 +256,6 @@ export default function ApplyList() {
                                           <option value="Pre-ex Coverage Not Included">
                                             Pre-ex Coverage Not Included
                                           </option>
-                                          {/* {this.state.scriptN?.map(
-                                            allScript => (
-                                              <option
-                                                value={allScript?._id}
-                                                key={allScript?._id}
-                                              >
-                                                {allScript?.scriptName}
-                                              </option>
-                                            )
-                                          )} */}
                                         </CustomInput>
                                       </div>
                                     </div>
@@ -361,7 +272,7 @@ export default function ApplyList() {
                                   </Col>
                                 </Row>
                               </TabPane>
-                              <TabPane tabId="2">
+                              {/* <TabPane tabId="2">
                                 <Row>
                                   <Col lg="12" sm="12">
                                     <ul className="tabList">
@@ -419,6 +330,136 @@ export default function ApplyList() {
                                         new sickness coverage.
                                       </p>
                                     </div>
+                                  </Col>
+                                </Row>
+                              </TabPane> */}
+                              <TabPane tabId="2">
+                                <h2 className="text-center">
+                                  Visitor Information
+                                </h2>
+                                <Row className="px-3">
+                                  <Col lg="6" md="6" sm="12">
+                                    <div className="p-1">
+                                      <div>
+                                        <label>First Name</label>
+                                        <input
+                                          className="form-control"
+                                          type="text"
+                                          placeholder="First Name "
+                                          name="trip_startdate"
+                                          value={trip_startdate}
+                                          onChange={handleChange}
+                                        />
+                                      </div>
+                                      <div>
+                                        <label>Date Of Birth</label>
+                                        <select id="sel" name="sel">
+                                          <option>item 1</option>
+                                          <option>item 2</option>
+                                          <option>item 3</option>
+                                          <option>item 4</option>
+                                        </select>
+                                      </div>
+                                      <div className="mt-2">
+                                        <CustomInput
+                                          type="select"
+                                          name="departure"
+                                          required
+                                          defaultValue=""
+                                          value={departure}
+                                          onChange={e => {
+                                            setDeparture(e.target.value);
+                                          }}
+                                        >
+                                          <option value="" disabled>
+                                            Departure
+                                          </option>
+                                          <option value="Pre-ex Coverage Included">
+                                            Pre-ex Coverage Included
+                                          </option>
+                                          <option value="Pre-ex Coverage Not Included">
+                                            Pre-ex Coverage Not Included
+                                          </option>
+                                        </CustomInput>
+                                      </div>
+                                      <div>
+                                        <h3>Additional Traveller Coverage</h3>
+
+                                        <Input
+                                          type="radio"
+                                          id="html"
+                                          name="additional_avg_cov"
+                                          value="additional_avg_cov"
+                                          className="m-2"
+                                        />
+                                        <label for="Yes">Yes</label>
+                                        <Input
+                                          type="radio"
+                                          id="html"
+                                          name="additional_avg_cov"
+                                          value="additional_avg_cov"
+                                          className="m-2"
+                                        />
+                                        <label for="No">No</label>
+                                      </div>
+                                    </div>
+                                  </Col>
+                                  <Col lg="6" md="6" sm="12">
+                                    <div className="p-1">
+                                      <div>
+                                        <label>Last Name</label>
+                                        <input
+                                          className="form-control"
+                                          placeholder="Last Name"
+                                          type="text"
+                                          name="trip_startdate"
+                                          value={trip_startdate}
+                                          onChange={handleChange}
+                                        />
+                                      </div>
+                                      <div>
+                                        <label>Trip End Time</label>
+                                        <input
+                                          className="form-control"
+                                          type="date"
+                                          name="Trip End Time"
+                                          onChange={handleChange}
+                                        />
+                                      </div>
+
+                                      <div className="mt-2">
+                                        <CustomInput
+                                          type="select"
+                                          name="departure"
+                                          required
+                                          defaultValue=""
+                                          value={departure}
+                                          onChange={e => {
+                                            setDeparture(e.target.value);
+                                          }}
+                                        >
+                                          <option value="" disabled>
+                                            Destination
+                                          </option>
+                                          <option value="Pre-ex Coverage Included">
+                                            Pre-ex Coverage Included
+                                          </option>
+                                          <option value="Pre-ex Coverage Not Included">
+                                            Pre-ex Coverage Not Included
+                                          </option>
+                                        </CustomInput>
+                                      </div>
+                                    </div>
+                                  </Col>
+                                  <Col lg="6" md="6" sm="6">
+                                    {/* <button className="btn get-btn">
+                                      Previous
+                                    </button> */}
+                                  </Col>
+                                  <Col lg="6" md="6" sm="6">
+                                    <button className="btn get-btn">
+                                      Next
+                                    </button>
                                   </Col>
                                 </Row>
                               </TabPane>
